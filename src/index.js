eb20c84344d5d99d31f02f28473aeddad0dc0548
Targas <targas@pr8.com.br>
2020-06-19 13:30:38 -0300

    composing and piping

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+import { compose, pipe } from 'lodash/fp'
+
 let input = '     Javascript     '
 let output = '<div>' + input.trim() + '</div>'
 
@@ -5,4 +7,7 @@ const trim = str => str.trim()
 const wrap = str => `<div> ${str} </div>`
 const lc = str => str.toLowerCase()
 
-const result = wrap(lc(trim(input)))
+const transform = pipe(trim, lc, wrap)
+transform(input)
+
+// const result = wrap(lc(trim(input)))
