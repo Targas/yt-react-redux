faf6d004110360b761f9b9809a8ca0f26068d1fe
Targas <targas@pr8.com.br>
2020-06-19 13:25:29 -0300

    functional composition

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
-setTimeout(() => console.log('Hello'), 1000)
+let input = '     Javascript     '
+let output = '<div>' + input.trim() + '</div>'
+
+const trim = str => str.trim()
+const wrap = str => `<div> ${str} </div>`
+const lc = str => str.toLowerCase()
+
+const result = wrap(lc(trim(input)))
