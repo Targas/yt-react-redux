a7c6b0b93492bf40264e915fb7f52e88aa80f2e8
Targas <targas@pr8.com.br>
2020-06-19 13:41:42 -0300

    Currying

--- src/index.js
+++ src/index.js
@@ -4,10 +4,10 @@ let input = '     Javascript     '
 let output = '<div>' + input.trim() + '</div>'
 
 const trim = str => str.trim()
-const wrap = str => `<div> ${str} </div>`
+const wrap = type => str => `<${type}> ${str} </${type}>`
 const lc = str => str.toLowerCase()
 
-const transform = pipe(trim, lc, wrap)
-transform(input)
+const transform = pipe(trim, lc, wrap('div'))
+console.log(transform(input))
 
-// const result = wrap(lc(trim(input)))
+// const result = wrapInDiv(lc(trim(input)))
