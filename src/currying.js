3096d9f962695dae0153c338588326ef11cd0f09
Targas <targas@pr8.com.br>
2020-06-19 22:32:03 -0300

    redux

--- src/currying.js
+++
@@ -1,9 +0,0 @@
-function add(a, b) {
-    return function(b) {
-        return a + b
-    }
-}
-
-const add2 = a => b => a + b
-
-add(1)(5)
--- src/index.js
+++ src/index.js
@@ -1,14 +1,1 @@
-import { produce } from 'immer'
-
-let book = { title: 'Fight Club' }
-
-function publish(book) {
-    produce(book, draftBook => {
-        draftBook.isPublished = true 
-    })
-}
-
-let updated = publish(book)
-
-console.log(book)
-console.log(updated)
+console.log("Hello World!");
