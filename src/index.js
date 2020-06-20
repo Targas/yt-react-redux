34764fe225240f80f2918a113ce06c27d9615a32
Targas <targas@pr8.com.br>
2020-06-19 22:07:00 -0300

    immutable

--- src/index.js
+++ src/index.js
@@ -1,15 +1,13 @@
-const numbers = [1,2,3]
+import { map } from 'immutable'
 
-const index = numbers.indexOf(2)
-const added = [
-    ...numbers.slice(0, index),
-    4,
-    ...numbers.slice(index)
-]
-console.log(added)
+let book = new Map({
+    title: 'Fight Club'
+})
 
-const removed = numbers.filter(n => n !== 2)
-console.log(removed)
+function publish(book) {
+    book.set('isPublished', true)
+}
 
-const updated = numbers.map(n => n === 2 ? 20 : n)
-console.log(updated)
+book = publish(book)
+
+console.log(book.toJs())
