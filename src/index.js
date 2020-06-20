154731db2c1e0b7741a92818c78e85abcdc38c60
Targas <targas@pr8.com.br>
2020-06-20 00:30:02 -0300

    subscribe

--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
 import store from './store'
 
+const unsubscribe = store.subscribe(() => {
+    console.log('store changed!', store.getState())
+})
+
 store.dispatch({
     type: 'bugAdded',
     payload: {
         description: 'bug 1'
     }
 })
+
+unsubscribe()
 
 store.dispatch({
     type: 'bugRemoved',
